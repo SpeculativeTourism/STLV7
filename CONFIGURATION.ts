4c1287c96ec8f7df6c215340561f4df41ff7fa02
Mushon Zer-Aviv <mushon@shual.com>
2022-11-14 17:05:11 +0200

    Update CONFIGURATION.ts

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -1,6 +1,6 @@
-export const AIRTABLE_BASE = 'appuAa65B1iKwaXXb';
+export const AIRTABLE_BASE = 'app0TC2V0x3xqmA3I';
 export const AIRTABLE_API_KEY = 'keysbVnYZk16tqIT0';
-export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shrynWjEy8fsje5t0';
+export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shrJCs8zJBx293MLW';
 
 export const MAPBOX_STYLE = 'mapbox://styles/atlasmedliq/cl59k9bun000y14rzt3yy5ljt';
 export const MAPBOX_BASE_STYLE = 'mapbox://styles/atlasmedliq/ckiodibg82x1k17tcof8e9pmd';
